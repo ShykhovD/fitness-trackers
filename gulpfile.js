f77ba7ba9caa0dc48f8a931a1632d615c0620792
Dima <dima220198@gmail.com>
2022-08-03 12:11:23 +0300

    Finish promo section

--- gulpfile.js
+++ gulpfile.js
@@ -7,31 +7,30 @@ const cleanCSS = require('gulp-clean-css');
 
 
 gulp.task('server', function() {
-    browserSync.init({
+    browserSync({
         server: {
             baseDir: "src"
         }
     });
+
+    gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task('styles', function(){
-    return gulp.src("src/sass/*.+(scss|sass)")
+    return gulp.src("src/sass/**/*.+(scss|sass)")
             .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
             .pipe(rename({
-                prefix: "",
-                suffix: ".min",
+                prefix: '',
+                suffix: '.min',
             }))
-            .pipe(autoprefixer({
-                cascade: false
-            }))
+            .pipe(autoprefixer())
             .pipe(cleanCSS({compatibility: 'ie8'}))
             .pipe(gulp.dest("src/css"))
             .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"))
-    gulp.watch("src/*.html").on("change", browserSync.reload);
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel("styles"))
 });
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
